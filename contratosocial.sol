e21b13304b69fbadafb1460a4324a8dfa4fce129
Sahelê Felicio <67347282+SaheleFelicio@users.noreply.github.com>
2020-06-29 16:59:41 -0300

    cto social
    Cláusulas básicas e função de simular aumento de capital com a participação do majoritário

---
+++ contratosocial.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.6.10;
+
+contract ContratoSocial
+{
+    string public socioUm;
+    string public socioDois;
+    string public sede;
+    string public administrador;
+    uint256 public capitalSocial;
+    uint256 public capitalSocioUm;
+    uint256 public capitalSocioDois;
+    
+    constructor (
+        string memory paramsocioUm,
+        string memory paramsocioDois,
+        string memory paramsede,
+        string memory paramadministrador,
+        uint256 valorDoCapitalSocial,
+        uint256 paramcapitalSocioUm,
+        uint256 paramcapitalSocioDois
+        )
+        public 
+        {
+            socioUm = paramsocioUm;
+            socioDois = paramsocioDois;
+            sede = paramsede;
+            administrador = paramadministrador;
+            capitalSocial = valorDoCapitalSocial;
+           capitalSocioUm = paramcapitalSocioUm;
+           capitalSocioDois = paramcapitalSocioDois;
+        }
+        function simulaAumentoCapitalSocioUm (uint256 aumentoDeCapital) public view returns (uint256 aumentoCapitalSocioUm)
+        {
+            aumentoCapitalSocioUm = aumentoDeCapital*capitalSocioUm/capitalSocial;
+            return aumentoCapitalSocioUm;
+        }
+                 
+}
+     
